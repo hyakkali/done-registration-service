28b6322cdb1ac62195d3849edf6913c8f8a456d0
Hemanth <hemanth.yakkali16@gmail.com>
2022-01-15 16:37:48 -0500

    cleaned up code and removed unused classes

--- src/main/java/com/done/RegistrationService/utils/LoggerUtil.java
+++ src/main/java/com/done/RegistrationService/utils/LoggerUtil.java
@@ -1,7 +1,5 @@
 package com.done.RegistrationService.utils;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.logging.Logger;
 
 public class LoggerUtil {
@@ -12,42 +10,5 @@ public class LoggerUtil {
             String message
     ) {
         LOGGER.info(String.format("[%s]: %s", clazz.getSimpleName(), message));
-    }
-
-    public static void warn(
-            Class<?> clazz,
-            String message
-    ) {
-        LOGGER.warning(String.format("[%s]: %s", clazz.getSimpleName(), message));
-    }
-
-    public static void logError(
-            Class<?> clazz,
-            String message
-    ) {
-        LOGGER.severe(String.format("[%s]: %s", clazz.getSimpleName(), message));
-    }
-
-    public static void logError(
-            Class<?> clazz,
-            Throwable exception
-    ) {
-        String errorStackTrace = stackTraceToString(exception);
-        LOGGER.severe(String.format("[%s]: %s", clazz.getSimpleName(), errorStackTrace));
-    }
-
-    public static void logError(
-            Class<?> clazz,
-            String message,
-            Throwable exception
-    ) {
-        String errorStackTrace = stackTraceToString(exception);
-        LOGGER.severe(String.format("[%s]: %s\n%s", clazz.getSimpleName(), message, errorStackTrace));
-    }
-
-    private static String stackTraceToString(Throwable exception) {
-        StringWriter sw = new StringWriter();
-        exception.printStackTrace(new PrintWriter(sw));
-        return sw.toString();
     }
 }
